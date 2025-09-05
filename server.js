86b6c42c3490627c29c03e17b6312f9cde22743b
shadowmoonezios <agushifelde@hotmail.com>
2025-09-05 21:23:59 +0300

    Updated MongoDB connection string to include options for better compatibility.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/personal_financial_management?retryWrites=true', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/personal_financial_management?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
 
