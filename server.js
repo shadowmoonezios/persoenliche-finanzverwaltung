5151d4222fca0f890737020b4aea79d8ace51b04
shadowmoonezios <agushifelde@hotmail.com>
2025-09-05 21:23:32 +0300

    Update MongoDB connection string to use environment variable for improved security.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/personal_financial_management?retryWrites=true', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/personal_financial_management?retryWrites=true', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB verbunden'))
     .catch(err => console.error(err));
 
