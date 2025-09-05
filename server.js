96051b75d68f823ff51f4472164f74ab6e3f5653
shadowmoonezios <agushifelde@hotmail.com>
2025-09-05 21:23:01 +0300

    Fix typo in MongoDB connection string for consistency

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/persoenliche_finanzverwaltung', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/personal_financial_management', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB verbunden'))
     .catch(err => console.error(err));
 
